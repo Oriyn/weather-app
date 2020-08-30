4bac2fb55fd1372ddaeadd16a93291e75147323a
Gabe Haggard <gabe.haggard@panerabread.com>
2020-08-30 13:10:44 -0500

    Backend get weather endpoint returning data from openweathermap

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
+const axios = require('axios').default;
+
+export class LocationDto {
+  url: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private apiKey = 'cf002751564a4c78f5f7ed479f1b9ba3';
+  constructor(private readonly appService: AppService) {
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Post('/weather')
+  async getWeather(@Body() weatherObj: LocationDto) {
+    return axios.get(`${weatherObj.url}${this.apiKey}`).then((response) => {
+      return response.data;
+    }).catch((error) => {
+      console.log('error', error);
+    });
   }
 }
